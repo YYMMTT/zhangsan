0af93d02773ba380a6fbf5d6a43def412912957a
ymt <yanjinmting@163.com>
2020-04-07 11:51:21 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,47 @@
+"""
+print("hello world") # 字符串
+print(123) # 整数
+print(23.2) # 小数
+print(True) # 布尔值
+print(())   # 元组
+print([])   # 数组
+print({})   # 字典
+锄禾日当午
+汗滴禾下土
+
+
+print("好好",23,23,5)
+print("你好","北京") # 字符串的拼接
+print("哈哈哈"*100)
+print(1+2)
+print(2>3)
+# 变量
+# 赋值
+name="张三" #把张三这个值赋值给了名字叫name这个变量
+print(name)
+"""
+# a=input("请输入：")
+# print("你输入的内容：",a)
+
+# 数据格式的转换
+# print(type("哈哈"))
+# print(type(123))
+# print(type(12.3))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# a=int(input("请输入："))
+# b=int(input("请输入："))
+# print("值",a+b)
+# a="哈哈哈"
+# print(len(a))
+"""
+输入两端内容，并且计算他们的长度之和
+"""
+a=len(input("请输入："))
+b=len(input("请输入："))
+
+print("输入内容的长度和：",a+b)
+
+ 
